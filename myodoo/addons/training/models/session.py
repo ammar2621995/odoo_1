0778ad55cc643314b33429278787c239d216c062
ammar2621995 <81688712+ammar2621995@users.noreply.github.com>
2022-03-10 13:06:53 +0200

    create session class , x,ml

---
+++ myodoo/addons/training/models/session.py
@@ -0,0 +1,17 @@
+from email.policy import default
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class CourseSession (models.Model):
+    _name = "training_session" #techncal name
+    _description = "second version"
+    _order = "id" #way to order instance of this class
+
+    name = fields.Char(string= "Name" , readonly = False , copy = False)
+    content = fields.Text("Content")
+    planned_date = fields.Datetime(string = "Planned Date")
+    actual_date  = fields.Datetime(string = "Actual Date" , readonly = True)
